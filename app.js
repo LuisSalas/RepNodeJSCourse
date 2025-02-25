a8bd32a876ee0c947f9de2910bce5a517423b70a
LuisSalas <lasalasp@gmail.com>
2025-02-25 09:32:15 -0600

    Routing requests

--- app.js
+++ app.js
@@ -7,7 +7,17 @@ const http = require("http");
  * Create an HTTP server that listens for requests and logs the request object to the console.
  */
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const url = req.url; // Get the URL from the request object
+  if (url === "/") {
+    res.write("<html>"); // Start the response body
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>"); // End the response body
+    return res.end(); // Send the response
+  }
+  //console.log(req.url, req.method, req.headers);
   //process.exit(); // This will stop the server after the first request.
   res.setHeader("Content-Type", "text/html"); // Set the response header
   res.write("<html>"); // Start the response body

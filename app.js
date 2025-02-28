52d23753e51b267746cfd423be03071069d743b8
LuisSalas <lasalasp@gmail.com>
2025-02-28 10:23:18 -0600

    Adding a 404 error page

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  //   res.status(404);
+  res.status(404).send("<h1>Page not found!</h1>");
+});
+
 app.listen(3000);
 
 // The  app.use()  method is used to add a middleware function to the middleware stack. The middleware function is executed for every incoming request.

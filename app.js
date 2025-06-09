1cfc0f09bfd9bb4986df945e0b4cf628fabbda54
LuisSalas <lasalasp@gmail.com>
2025-06-09 17:11:14 -0600

    Refactor of app.js

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
 const path = require("path");
 
 const express = require("express");
-const bodyParser = require("body-parser");
-// const handlebars = require("express-handlebars"); // import the express-handlebars package
 
 const app = express(); // create an express application
 
-// app.engine(
-//   "hbs",
-//   handlebars({
-//     layoutsDir: "views/layouts/",
-//     defaultLayout: "main-layout",
-//     extname: "hbs",
-//   })
-// ); // set the view engine to handlebars
-// app.set("view engine", "hbs"); // set the view engine to handlebars
-// app.set("view engine", "pug"); // set the view engine to pug
 app.set("view engine", "ejs"); // set the view engine to ejs
 app.set("views", "views"); // set the views directory
 
 const adminData = require("./routes/admin"); // import the admin routes
 const shopRoutes = require("./routes/shop"); // import the shop routes
 
-app.use(bodyParser.urlencoded({ extended: false })); // to parse the body of incoming requests
+app.use(express.urlencoded({ extended: true })); // to parse the body of incoming requests
 app.use(express.static(path.join(__dirname, "public"))); // to serve static files such as images, CSS files, and JavaScript files
 
 app.use("/admin", adminData.routes); // use the admin routes for all routes starting with /admin path of the incoming request (e.g., /admin/add-product)
